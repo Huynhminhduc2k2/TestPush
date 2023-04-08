09c45e6626577d925d39181c855beb6ed99f87eb
Huynhminhduc2k2 <hmduc20@clc.fitus.edu.vn>
2023-04-08 15:44:37 +0700

    First Commit

---
+++ Java_98/src/view/ViDu.java
@@ -0,0 +1,54 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JDesktopPane;
+import javax.swing.JTabbedPane;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import javax.swing.JSplitPane;
+
+public class ViDu extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViDu frame = new ViDu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ViDu() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JSplitPane splitPane = new JSplitPane();
+		contentPane.add(splitPane);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		contentPane.add(desktopPane);
+		desktopPane.setLayout(new BorderLayout(0, 0));
+	}
+
+}
